71f056c4799ae942664417d56c67ab1885a0e7d5
Narayanaswamy-m <venkatanarayanamedikonda@gmail.com>
2026-02-25 09:23:21 +0530

    changes

--- roboshop.sh
+++ roboshop.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 SG_ID="sg-026eda32a5a9ca5d0"
 AMI_ID="ami-0220d79f3f480ecf5"
-for instance in $@
-do 
- Instance_id= $( aws ec2 run-instances \
-  --image-id $AMI_ID \
-  --instance-type t3.micro \
-  --security-group-ids $SG_ID \
-  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-  --query "Instances[0].Instanceid" \
-  --output text )
-  if [ $instance == :: frontend ]
-   IP=$(
-    aws ec2 describe-instances \
-  --instance-ids $Instance_id \
-  --query "Reservations[*].Instances[*].PrivateIpAddress" \
-  --output text
-  )
 
-  else 
-   IP=$(
-    aws ec2 describe-instances \
-  --instance-ids $Instance_id \
-  --query "Reservations[*].Instances[*].PublicIpAddress" \
-  --output text
-  )
-  fi 
+for instance in "$@"; do
+  instance_id=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type t3.micro \
+    --security-group-ids "$SG_ID" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Instances[0].InstanceId" \
+    --output text)
+
+  if [ "$instance" == "frontend" ]; then
+    IP=$(aws ec2 describe-instances \
+      --instance-ids "$instance_id" \
+      --query "Reservations[*].Instances[*].PrivateIpAddress" \
+      --output text)
+  else
+    IP=$(aws ec2 describe-instances \
+      --instance-ids "$instance_id" \
+      --query "Reservations[*].Instances[*].PublicIpAddress" \
+      --output text)
+  fi
+
   echo "ipaddress :: $IP"
 done
 
